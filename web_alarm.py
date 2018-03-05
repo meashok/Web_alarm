bfcae70d164cb854c24f614052b7c565f1564cd3
Ashok Yadav <meashokyadav@gmail.com>
2018-03-05 12:55:57 +0530

    Add files via upload

---
+++ web_alarm.py
@@ -0,0 +1,11 @@
+import time
+import webbrowser
+
+total_breaks=3
+break_count=0
+
+print("This program starts at"+time.ctime())
+while(break_count<total_breaks):
+    time.sleep(5)
+    webbrowser.open("https://www.youtube.com/watch?v=Z9eMk051dYg")
+    break_count=break_count+1
